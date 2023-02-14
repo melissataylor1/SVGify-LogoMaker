190a4e0c306699edbd053ff7bc7020c469735bf7
m1taylor <m1taylor@ryerson.ca>
2023-02-14 01:14:23 -0500

    trouble shooting with package lock json

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const inquirer = require('inquirer');
+
+const {prompt} = require('inquirer');
+const {questions} = require('./lib/questions');
 const {makeSVG} = require('./lib/makeSVG');
 
-//questions
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'logoName',
-      message: 'Please enter text for SVG Logo (MAX 3 characters)',
-    
-    },
-    {
-      type: 'input',
-      name: 'textColour',
-      message: `Please enter text colour keyword or a hexadecimal number for the logo's test color`,
-    },
-    {
-      type: 'input',
-      name: 'logoColour',
-      message: `Please enter a colour keyword or a hexadecimal number for the logo's background colour`,
-    },
-    {
-      type: 'list',
-      name: 'logoShape',
-      message: `Please choose logo shape`,
-      choices: ['triangle', 'circle', 'square'],
-    },
-  ])
-
-  //pulling fs code that generates logo in seperate file for clarity
-  //this calls the seperate file
-  .then(makeSVG);
+prompt(questions).then(makeSVG);
