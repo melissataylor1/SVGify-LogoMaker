a73506057cfac35c952634095764a22e3e543670
m1taylor <m1taylor@ryerson.ca>
2023-02-14 12:12:19 -0500

    final troubleshooting, it is working now

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,61 +1,3 @@
-const { Circle, Triangle, Square } = require('../lib/shapes');
-
-
-describe("Circle", () => {
-    it("should generate a svg file of a circle with specifications matching user input", () => {
-        const shapeColor = "black";
-        const textColor = "green";
-        const text = "NAC";
-        const correctCircle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
-        //     <text x="150" y="120" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-        </svg>`
-
-        const makeCircle = () => new Circle(shapeColor, textColor, text).render();
-
-        expect(makeCircle()).toEqual(correctCircle);
-    })
-})
-
-
-describe("Triangle", () => {
-    it("should generate a svg file of a triangle with specifications matching user input", () => {
-        const shapeColor = "black";
-        const textColor = "green";
-        const text = "NAC";
-        const correctTriangle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="160,10 290,200 10,200" style="fill:${shapeColor};stroke:purple;stroke-width:1" />
-        <text x="155" y="150" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-        </svg>`
-
-        const makeTriangle = () => new Triangle(shapeColor, textColor, text).render();
-
-        expect(makeTriangle()).toEqual(correctTriangle);
-    })
-})
-
-
-describe("Square", () => {
-    it("should generate a svg file of a square with specifications matching user input", () => {
-        const shapeColor = "black";
-        const textColor = "green";
-        const text = "NAC";
-        const correctSquare = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-        <rect width="200" height="200" fill="${shapeColor}" />
-        <text x="100" y="115" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-        </svg>`
-
-        const makeSquare = () => new Square(shapeColor, textColor, text).render();
-
-        expect(makeSquare()).toEqual(correctSquare);
-    })
-})
-
-
-
-
-/*
-
 const { Shape, Circle, Triangle, Square } = require('../lib/shapes');
 
 //Generated SVGs should match input accurately for each shape
@@ -138,3 +80,60 @@ describe('Validate input length', () => {
       expect(shape).toThrowError(err);
     });
   });
+/* const { Circle, Triangle, Square } = require('../lib/shapes');
+
+
+describe("Circle", () => {
+    it("should generate a svg file of a circle with specifications matching user input", () => {
+        const shapeColor = "black";
+        const textColor = "green";
+        const text = "NAC";
+        const correctCircle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="80" fill="${shapeColor}" />
+        //     <text x="150" y="120" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>`
+
+        const makeCircle = () => new Circle(shapeColor, textColor, text).render();
+
+        expect(makeCircle()).toEqual(correctCircle);
+    })
+})
+
+
+describe("Triangle", () => {
+    it("should generate a svg file of a triangle with specifications matching user input", () => {
+        const shapeColor = "black";
+        const textColor = "green";
+        const text = "NAC";
+        const correctTriangle = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="160,10 290,200 10,200" style="fill:${shapeColor};stroke:purple;stroke-width:1" />
+        <text x="155" y="150" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>`
+
+        const makeTriangle = () => new Triangle(shapeColor, textColor, text).render();
+
+        expect(makeTriangle()).toEqual(correctTriangle);
+    })
+})
+
+
+describe("Square", () => {
+    it("should generate a svg file of a square with specifications matching user input", () => {
+        const shapeColor = "black";
+        const textColor = "green";
+        const text = "NAC";
+        const correctSquare = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="200" height="200" fill="${shapeColor}" />
+        <text x="100" y="115" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>`
+
+        const makeSquare = () => new Square(shapeColor, textColor, text).render();
+
+        expect(makeSquare()).toEqual(correctSquare);
+    })
+})
+
+
+*/
+
+
